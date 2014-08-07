2af161b567ea9f09cd8cad39a70340c78dbbd5bd
Daniel Abrahamsson <hamsson@gmail.com>
2014-08-07 20:58:45 +0200

    Add basic error handling

--- slowgrep.rs
+++ slowgrep.rs
@@ -1,12 +1,11 @@
-use std::io::File;
-use std::io::BufferedReader;
+use std::io::{File, BufferedReader};
 use std::os;
 
 fn main() {
   let args: Vec<String> = os::args();
   match args.as_slice() {
     [ref _prog, ref pattern, ..paths] => run_grep(pattern.as_slice(), paths),
-    _                         => print_usage()
+    _                                 => print_usage()
   }
 }
 
@@ -24,9 +23,14 @@ fn grep(pathStr: &str, pattern: &str) {
   let path = Path::new(pathStr);
   let mut reader = BufferedReader::new(File::open(&path)); 
   for line in reader.lines() {
-    let haystack = line.unwrap();
-    if haystack.as_slice().contains(pattern) {
-      print!("{}: {}", pathStr, haystack);
+    if line.is_ok() {
+      let haystack = line.unwrap();
+      if haystack.as_slice().contains(pattern) {
+        print!("{}: {}", pathStr, haystack);
+      }
+    } else {
+      std::io::stderr().write_str(format!("slowgrep: Failed to process {}\n", pathStr).as_slice()).unwrap();
+      break;
     }
   }
 }
