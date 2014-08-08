0efe6a8e02a81104d517d49d63dd1baa62b81a87
Daniel Abrahamsson <hamsson@gmail.com>
2014-08-08 20:12:38 +0200

    First attempt at erlang style communication

--- slowgrep.rs
+++ slowgrep.rs
@@ -14,18 +14,42 @@ fn print_usage() {
 }
 
 fn run_grep(pattern: &String, paths: &[String]) {
+  let (parent_tx, parent_rx) = channel();
+  let workers = Vec::from_fn(3, |idx| {
+    let (child_tx, child_rx): (Sender<Option<String>>, Receiver<Option<String>>) = channel();
+    let child_pattern = pattern.clone();
+    let child_parent_tx = parent_tx.clone();
+    spawn(proc() { child(idx, child_parent_tx, child_rx, child_pattern) });
+    child_tx
+  });
+
   for path in paths.iter() {
-    let pattern2 = pattern.clone();
-    let path2 = path.clone();
-    spawn(proc() {
-      grep(path2.as_slice(), pattern2.as_slice());
-    });
+    let idx = parent_rx.recv();
+    let worker = workers.get(idx);
+    worker.send(Some(path.clone()));
+  };
+  for worker in workers.iter() {
+    worker.send(None);
+  }
+}
+
+fn child(idx: uint, tx:Sender<uint>, rx:Receiver<Option<String>>, pattern: String) {
+  let slice = pattern.as_slice();
+  loop {
+    if tx.send_opt(idx).is_ok() {
+      match rx.recv() {
+        Some(path) => grep(path.as_slice(), slice),
+        None => return // Done
+      }
+    } else {
+      return; // Parent died
+    }
   }
 }
 
 fn grep(pathStr: &str, pattern: &str) {
   let path = Path::new(pathStr);
-  let mut reader = BufferedReader::new(File::open(&path)); 
+  let mut reader = BufferedReader::new(File::open(&path));
   for line in reader.lines() {
     if line.is_ok() {
       let haystack = line.unwrap();
